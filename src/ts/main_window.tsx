2193d730e808a7f7ad2bf20605ff4ebc58c8694d
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-02-20 14:22:46 +0900

    lkasdf

--- src/ts/main_window.tsx
+++ src/ts/main_window.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import { hot } from 'react-hot-loader/root';
+import { hot } from 'react-hot-loader';
 
 
 function MainWindow (props: {}) {
   return (
-    <h1>Hello, world!</h1>
+    <h1>Hello, world! asdfasdfasdf aefawefaw</h1>
   );
 }
 
 
-const HotMainWindow = hot(MainWindow);
+const HotMainWindow = hot(module)(MainWindow);
 
 
 export { HotMainWindow as MainWindow };
